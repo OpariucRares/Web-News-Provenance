228dc64ba1faf46079ae13b2ed2a6314aae769cf
OpariucRares <opariucraresioan@gmail.com>
2025-02-02 21:50:55 +0200

    updated deploy.yml

--- frontend/react-vite/vite.config.ts
+++ frontend/react-vite/vite.config.ts
@@ -5,10 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Web-News-Provenance/",
   resolve: {
     alias: {
       "date-fns": path.resolve(__dirname, "node_modules/date-fns"),
-      base: "/Web-News-Provenance/",
     },
   },
 });
