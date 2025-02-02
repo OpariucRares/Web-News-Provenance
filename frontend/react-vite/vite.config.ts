2f1bab0962d29090592ca5d01d56b4c38b8fb0f5
OpariucRares <opariucraresioan@gmail.com>
2025-02-02 19:15:53 +0200

    deploy frontend

--- frontend/react-vite/vite.config.ts
+++ frontend/react-vite/vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'date-fns': path.resolve(__dirname, 'node_modules/date-fns'),
+      "date-fns": path.resolve(__dirname, "node_modules/date-fns"),
+      base: "/Web-News-Provenance",
     },
   },
-})
+});
