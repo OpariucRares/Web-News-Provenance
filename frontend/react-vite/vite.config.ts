3f847cf3c690e8024219b0e84b39a5abe7f7070e
OpariucRares <opariucraresioan@gmail.com>
2025-02-03 13:53:50 +0200

    change to azure

--- frontend/react-vite/vite.config.ts
+++ frontend/react-vite/vite.config.ts
@@ -5,6 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   define: {
     "process.env": process.env,
   },
