99b60598d4829d370f5e6114ad7d081984700550
OpariucRares <opariucraresioan@gmail.com>
2025-02-03 15:34:38 +0200

    changed to github pages

--- frontend/react-vite/vite.config.ts
+++ frontend/react-vite/vite.config.ts
@@ -1,17 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { exec } from "child_process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Web-News-Provenance/",
+  base: "/",
   define: {
     "process.env": process.env,
   },
   resolve: {
     alias: {
       "date-fns": path.resolve(__dirname, "node_modules/date-fns"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      plugins: [
+        {
+          name: "copy-cname",
+          closeBundle: () => {
+            exec("cp CNAME dist/CNAME", (err) => {
+              if (err) {
+                console.error("Error copying CNAME file:", err);
+              } else {
+                console.log("CNAME file copied to dist directory");
+              }
+            });
+          },
+        },
+      ],
     },
   },
 });
